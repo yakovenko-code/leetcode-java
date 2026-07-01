e2a933bb04f116eec0d828973599ce1343b42a2c
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-01 17:45:19 +0300

    ADD: 21. Merge Two Sorted Lists

---
+++ src/main/java/code/yakovenko/leetcode/easy/MergeTwoSortedLists.java
@@ -0,0 +1,30 @@
+package code.yakovenko.leetcode.easy;
+
+import code.yakovenko.leetcode.utils.ListNode;
+
+public final class MergeTwoSortedLists {
+
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		if (list1 == null) return list2;
+		if (list2 == null) return list1;
+
+		final ListNode mergedList = new ListNode();
+		ListNode current = mergedList;
+
+		while (list1 != null && list2 != null) {
+			if (list1.val <= list2.val) {
+				current.next = list1;
+				list1 = list1.next;
+			} else {
+				current.next = list2;
+				list2 = list2.next;
+			}
+
+			current = current.next;
+		}
+
+		current.next = list1 != null ? list1 : list2;
+
+		return mergedList.next;
+	}
+}
