216d53643791a2eee7d8121e01b56851bc119507
Yakovenko Ilya <yakovenko.code@yandex.ru>
2026-07-03 15:54:27 +0300

    FIX: 70. Climbing Stairs

--- src/main/java/code/yakovenko/leetcode/easy/ClimbingStairs.java
+++ src/main/java/code/yakovenko/leetcode/easy/ClimbingStairs.java
@@ -5,15 +5,14 @@ public final class ClimbingStairs {
 	public int climbStairs(int n) {
 		if (n <= 2) return n;
 
-		final int[] data = new int[n];
+		int prev2 = 1;
+		int prev1 = 2;
 
-		data[0] = 1;
-		data[1] = 2;
-
-		for (int i = 2; i < n; i++) {
-			data[i] = data[i - 1] + data[i - 2];
+		for (int i = 3; i <= n; i++) {
+			prev1 += prev2;
+			prev2 = prev1 - prev2;
 		}
 
-		return data[n - 1];
+		return prev1;
 	}
 }
